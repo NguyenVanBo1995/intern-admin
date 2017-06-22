828e2d2f0e1da8f4b0dc327c93c9694cd05389f5
DESKTOP-PAJF844\BO-BK <nguyenvanbo060395@gmail.com>
2017-06-22 14:27:25 +0700

    update

---
+++ database/migrations/2017_06_22_070918_creat_item_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->mediumText('description')->nullable();
+            $table->double('price');
+            $table->tinyInteger('type')->default(0);
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
--- database/migrations/2017_06_22_071602_create_item_table.php
+++ database/migrations/2017_06_22_071602_create_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemsTable extends Migration
+class CreateItemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,13 +18,14 @@ class CreateItemsTable extends Migration
             $table->string('name');
             $table->mediumText('description')->nullable();
             $table->double('price');
+            $table->tinyInteger('type')->default(0);
             $table->integer('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
 
-    /** 
+    /**
      * Reverse the migrations.
      *
      * @return void
